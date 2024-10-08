dbec8e45cbeaf6b92c0ec3435b4c78d0a5c50da8
Rajender kumar <kumar.rajender@rwit.io>
2024-10-08 13:05:08 +0530

    Feat: Remove the extra comments

--- src/main.ts
+++ src/main.ts
@@ -13,33 +13,20 @@ const calculateAge = () => {
   let years = today.getFullYear() - BirthDate.getFullYear();
   let months = today.getMonth() - BirthDate.getMonth();
   let days = today.getDate() - BirthDate.getDate();
-  
+
+  if (days < 0) {
+    months--;
+    days += new Date(today.getFullYear(), today.getMonth(), 0).getDate();
+  }
+
+  if (months < 0) {
+    years--;
+    months += 12;
+  }
+
   if (Result) {
     Result.innerHTML = `You are ${years} years, ${months} months, and ${days} days old.`;
   }
 };
 
 Button?.addEventListener("click", calculateAge);
-
-// function calculateAge() {
-//   const BirthDateInput = document.getElementById("birthdate").value;
-//   const birthdate = new Date(birthdateInput);
-//   const today = new Date();
-
-//   let years = today.getFullYear() - birthdate.getFullYear();
-//   let months = today.getMonth() - birthdate.getMonth();
-//   let days = today.getDate() - birthdate.getDate();
-
-//   // Adjust for negative months or days
-//   if (days < 0) {
-//       months--;
-//       days += new Date(today.getFullYear(), today.getMonth(), 0).getDate();
-//   }
-
-//   if (months < 0) {
-//       years--;
-//       months += 12;
-//   }
-
-//   document.getElementById("result").innerText = `You are ${years} years, ${months} months, and ${days} days old.`;
-// }
